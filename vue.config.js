a5033265cf7968f7a9da0651e19cbc0ca1004694
huang <hwguge6@gmail.com>
2021-05-10 16:47:06 +0800

    初始代码提交

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  css: {
+    loaderOptions: {
+      sass: {
+        // 全局引入变量和 mixin
+        additionalData: `
+          @import "@/assets/scss/variable.scss";
+          @import "@/assets/scss/mixin.scss";
+        `
+      }
+    }
+  }}
