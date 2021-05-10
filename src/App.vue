f470418ff88ab599c48aa951744a07504ed0dd3e
littlefox <hwguge6@gmail.com>
2021-05-10 17:08:28 +0800

    定义好tab组件

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <template>
   <m-header></m-header>
+  <tab></tab>
 </template>
 <script>
 import Header from '@/components/header/header'
+import Tab from '@/components/tab/tab'
 export default {
   components: {
-    MHeader: Header
+    MHeader: Header,
+    Tab
   }
 }
 </script>
