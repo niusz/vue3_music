255b90b27934cb12ee6d6e258cd7135cec904011
littlefox <163yourba@163.com>
2021-05-11 15:37:04 +0800

    增加自定义指令v-loading

--- src/main.js
+++ src/main.js
@@ -6,7 +6,8 @@ import lazyPlugin from 'vue3-lazy'
 
 // 引入全局样式文件
 import '@/assets/scss/index.scss'
+import loadingDirective from './components/base/loading/directive'
 
 createApp(App).use(store).use(router).use(lazyPlugin, {
 loading: require('@/assets/images/default.png')
-}).mount('#app')
+}).directive('loading', loadingDirective).mount('#app')
--- src/views/recommend.vue
+++ src/views/recommend.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="recommend">
+  <div class="recommend" v-loading:[loadingText]="loading">
     <scroll class="recommend-content">
       <div>
         <div class="slider-wrapper">
@@ -8,13 +8,12 @@
           </div>
         </div>
       <div class="recommend-list">
-        <h1 class="list-title">热门歌单推荐</h1>
+        <h1 class="list-title" v-show="!loading">热门歌单推荐</h1>
         <ul>
           <li
             v-for="item in albums"
             class="item"
             :key="item.id"
-            @click="selectItem(item)"
           >
             <div class="icon">
               <img width="60" height="60" v-lazy="item.pic">
@@ -48,7 +47,13 @@ export default {
   data() {
     return {
       sliders: [],
-      albums: []
+      albums: [],
+      loadingText: '正在载入'
+    }
+  },
+  computed: {
+    loading() {
+      return !this.sliders.length && !this.albums.length
     }
   },
   async created() {
